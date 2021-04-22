52b6c4127dabd78c4ad9e5280c83b4861ce60390
Marcos Porto <marcosporto99@gmail.com>
2021-04-22 16:02:43 -0300

    Update main.js

--- src/main.js
+++ src/main.js
@@ -17,5 +17,5 @@ var bot = new Bot(token);
 bot.login();
 // Heroku requires the process to bind to this port within 60 seconds or it is killed
 http.createServer(function(req, res) {
-  res.end('SLACK_POKER_BOT');
+  res.send({ master: bot });
 }).listen(3000)
